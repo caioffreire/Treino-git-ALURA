b404e345b6fc4a51ea218acd3434ac906d81e15e
caioffreire <caioffreire1@gmail.com>
2022-08-08 10:03:49 -0300

    voltando atrás no histórico

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("Aplicação está se conectando ao banco de dados, de mentirinha, é só um log.")
-// teste de console.log
-console.log("Banana banna banana.")
